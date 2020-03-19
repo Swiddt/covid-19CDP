82f8343239e55df412d85581e23452af60a019cd
swiddt <swiddt@gmail.com>
2020-03-19 20:43:05 +0100

    Update covid-19CDP.py

--- covid-19CDP.py
+++ covid-19CDP.py
@@ -10,11 +10,14 @@ class district:
   post = ''
   xpath = ''
 
-def scrab_xpath(url, xpath):
+def scrab_xpath(url, xpath, pre, post):
   page = requests.get(url)
   tree = html.fromstring(page.content)
   result = tree.xpath(xpath)[0]
-  
+  if not pre=='':
+    offset = result.find(pre)
+    result = result[len(pre)+offset:]
+
   value = ''
   for c in result:
     if not c.isdigit():
@@ -23,8 +26,11 @@ def scrab_xpath(url, xpath):
   
   print(value)
 
-url = 'https://www.kreis-re.de/Inhalte/Buergerservice/Gesundheit_und_Ernaehrung/Infektionsschutz/Coronavirus.asp'
-scrab_xpath(url, '//*[@id="id35557"]/ul[1]/li/strong/text()')
+#url = 'https://www.kreis-re.de/Inhalte/Buergerservice/Gesundheit_und_Ernaehrung/Infektionsschutz/Coronavirus.asp'
+#scrab_xpath(url, '//*[@id="id35557"]/ul[1]/li/strong/text()')
+url = 'http://www.aachen.de/DE/stadt_buerger/notfall_informationen/corona/aktuelles/index.html'
+pre = "Aktuell "
+scrab_xpath(url, '//*[@id="rahmeninhalt"]/div[3]/div[1]/div/div/read/ul[2]/li[1]/p/text()', pre, '')
 
 
 
